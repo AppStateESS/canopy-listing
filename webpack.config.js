41f0f15f1163de51fe9846e29a750c227edc6d92
Matt McNaney <mcnaneym@appstate.edu>
2019-05-20 13:19:00 -0400

    source map added.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,52 @@
 /* global __dirname */
 const path = require('path')
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
-  },
-  resolve: {
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", {loader: "sass-loader", options: {implementation: require("dart-sass")}}]
-      }, {
-        test: /\.jsx?/,
-        resolve: {extensions: ['.js', '.jsx']},
-        include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
+module.exports = (env, argv) => {
+  const inProduction = argv.mode === 'production'
+  const inDevelopment = argv.mode === 'development'
+
+  const settings = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'index.js',
+      libraryTarget: 'commonjs2'
+    },
+    resolve: {
+      alias: {
+        'react': path.resolve(__dirname, './node_modules/react'),
+        'react-dom': path.resolve(__dirname, './node_modules/react-dom')
       }
-    ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          use: [
+            "style-loader",
+            "css-loader", {
+              loader: "sass-loader",
+              options: {
+                implementation: require("dart-sass")
+              }
+            }
+          ]
+        }, {
+          test: /\.jsx?/,
+          resolve: {
+            extensions: ['.js', '.jsx']
+          },
+          include: path.resolve(__dirname, 'src'),
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      ]
+    }
   }
+  
+  if (inDevelopment) {
+    settings.devtool = 'source-map'
+  }
+  return settings
 }
